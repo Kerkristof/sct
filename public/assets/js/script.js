920beff8b4481fc6e542db44a373b1b14293f8b4
kerkristof <kerkristof@gmail.com>
2021-11-12 12:44:57 +0100

    Evolution de la nav-bar

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -2,4 +2,13 @@ $(document).ready(function(){
   $('.nav-burger').click(function(){
     $('.nav-links').toggleClass('responsive');
   });
+  $(window).scroll(function(){
+    if ($(this).scrollTop() > 20) {
+      $('.nav-bar').addClass('scroll');
+    }
+    else {
+      $('.nav-bar').removeClass('scroll');
+    }
+  });
+
 });
