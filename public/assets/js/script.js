8e614b61effe12309d608d0ffcfc3f66995639a1
kerkristof <kerkristof@gmail.com>
2021-11-17 09:38:21 +0100

    Mise à jour de la nav bar

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -2,6 +2,12 @@ $(document).ready(function(){
   $('.nav-burger').click(function(){
     $('.nav-links').toggleClass('responsive');
   });
+
+  $('.down-link').hover(function(){
+    $('.nav-sublinks', this).toggleClass('responsive');
+  });
+
+
   $(window).scroll(function(){
     if ($(this).scrollTop() > 20) {
       $('.nav-bar').addClass('scroll');
