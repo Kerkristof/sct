198c5fe7310a0690ed3a92c4360f801e77219eb7
kerkristof <kerkristof@gmail.com>
2021-11-25 14:26:20 +0100

    affichage des 5 derniers commentaires sur la page d accueil en mode connecté

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,17 +5,19 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\BlogCommentRepository;
 
 class HomeController extends AbstractController
 {
     /**
      * @Route("/", name="home")
      */
-    public function index(): Response
+    public function index(BlogCommentRepository $blog_repo): Response
     {
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
-        ]);
+      $last_blog_comments = $blog_repo->findBy([],['createdAt' => 'DESC'], 5);
+      return $this->render('home/index.html.twig', [
+          'blog_comments' => $last_blog_comments,
+      ]);
     }
     /**
      * @Route("/useful_link", name="useful_link")
