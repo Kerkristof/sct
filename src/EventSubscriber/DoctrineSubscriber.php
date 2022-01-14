05f965a547b598a75076f5fd4f04a60d9e7ca49c
kerkristof <kerkristof@gmail.com>
2022-01-14 14:37:36 +0100

    update notification by email

--- src/EventSubscriber/DoctrineSubscriber.php
+++ src/EventSubscriber/DoctrineSubscriber.php
@@ -3,26 +3,17 @@
 namespace App\EventSubscriber;
 
 use Doctrine\Bundle\DoctrineBundle\EventSubscriber\EventSubscriberInterface;
-use App\Entity\Event;
-use App\Entity\BlogComment;
-use App\Entity\ContactMessage;
 use Doctrine\ORM\Events;
 use Doctrine\Persistence\Event\LifecycleEventArgs;
 use App\Services\EmailService;
-use App\Repository\UserRepository;
-use SebastianBergmann\CodeCoverage\Driver\PHPDBG;
-
-
 
 class DoctrineSubscriber implements EventSubscriberInterface
 {
   private $email_service;
-  private $user_repo;
 
-  public function __construct(EmailService $email_service, UserRepository $user_repo)
+  public function __construct(EmailService $email_service)
   {
     $this->email_service = $email_service;
-    $this->user_repo = $user_repo;
   }
 
   // this method can only return the event names; you cannot define a
@@ -42,37 +33,7 @@ class DoctrineSubscriber implements EventSubscriberInterface
   private function emailNotifyer(string $action, LifecycleEventArgs $args): void
   {
     $entity = $args->getObject();
-    if ($entity instanceof BlogComment) {
-      // SEND AN EMAIL TO MEMBER WHEN A NEW BLOG IS PUBLISHED
-      $parameters = [
-        'subject' => 'Nouveau message sur le blog',
-        'content' => $entity->getAuthor()->getFirstname() . " " . $entity->getAuthor()->getName() ." vient de publier un nouveau post https://www.surfcastingturballais.fr/login ",
-        'role' => 'ROLE_MEMBER',
-        'recipients' => $this->user_repo->findAll(),
-      ];
-      $this->email_service->sendEmail($parameters);
-    }
-    if ($entity instanceof Event) {
-      // SEND AN EMAIL TO MEMBER WHEN A NEW EVENT IS PUBLISHED
-      $parameters = [
-        'subject' => 'Nouvel évenement sur le site',
-        'content' => $entity->getAuthor()->getFirstname() . " " . $entity->getAuthor()->getName() ." vient de publier un nouvel événement https://www.surfcastingturballais.fr/login ",
-        'role' => 'ROLE_MEMBER',
-        'recipients' => $this->user_repo->findAll(),
-      ];
-      $this->email_service->sendEmail($parameters);
-    }
-    if ($entity instanceof ContactMessage) {
-      // SEND EMAIL TO ADMIN WHEN A CONTACTMESSAGE IS RECEIVED
-      $parameters = [
-        'subject' => 'Nouvelle demande de contact reçue',
-        'content' =>"De " . $entity->getFirstname() . " " . $entity->getName() . " / email : " .
-        $entity->getEmail() . " / message: " . $entity->getContent() . " https://www.surfcastingturballais.fr/login ",
-        'role' => 'ROLE_ADMIN',
-        'recipients' => $this->user_repo->findAll(),
-      ];
-      $this->email_service->sendEmail($parameters);
-    }
+    $this->email_service->sendEmail($entity);
     return;
   }
 }
--- src/Services/EmailService.php
+++ src/Services/EmailService.php
@@ -2,35 +2,80 @@
 namespace App\Services;
 
 use Symfony\Component\Mailer\MailerInterface;
-use Symfony\Component\Mime\Email;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use App\Services\GrantedService;
+use App\Entity\Event;
+use App\Entity\BlogComment;
+use App\Entity\ContactMessage;
+use App\Repository\UserRepository;
 
 class EmailService
 {
   private $mailer;
   private $granted_service;
+  private $user_repo;
 
-/**
- * @param MailerInterface $mailer          [description]
- * @param GrantedService  $granted_service [description]
- */
-  public function __construct(MailerInterface $mailer, GrantedService $granted_service)
+ /**
+  * @param MailerInterface $mailer          [description]
+  * @param GrantedService  $granted_service [description]
+  * @param UserRepository  $user_repo       [description]
+  */
+  public function __construct(MailerInterface $mailer, GrantedService $granted_service, UserRepository $user_repo)
   {
     $this->mailer = $mailer;
     $this->granted_service = $granted_service;
+    $this->user_repo = $user_repo;
   }
 
-  public function sendEmail($parameters) : void
+  public function sendEmail($entity) : void
   {
-    $email = (new Email())
-    ->from('surfcastingturballais@lanco.ovh')
-    ->subject($parameters['subject'])
-    ->text($parameters['content']);
-    foreach ($parameters['recipients'] as $recipient) {
-      if ($this->granted_service->isGranted($recipient, $parameters['role'] )) {
-        $email->addTo($recipient->getEmail());
+    $recipients = $this->user_repo->findAll();
+    $email = (new TemplatedEmail());
+
+    if ($entity Instanceof ContactMessage) {
+      $email->from('surfcastingturballais@lanco.ovh')
+            ->subject('Une nouvelle demande de renseignement à consulter')
+            ->htmlTemplate('email_template/contact_message_notification.html.twig')
+            ->context([
+              'entity' => $entity,
+            ]);
+      foreach ($recipients as $recipient) {
+        if ($this->granted_service->isGranted($recipient, 'ROLE_ADMIN' )) {
+          $email->addTo($recipient->getEmail());
+        }
       }
+      $this->mailer->send($email);
     }
-    $this->mailer->send($email);
+
+    if ($entity Instanceof BlogComment) {
+      $email->from('surfcastingturballais@lanco.ovh')
+            ->subject('Un nouveau message vient d\'être publié sur le forum')
+            ->htmlTemplate('email_template/blog_comment_notification.html.twig')
+            ->context([
+              'entity' => $entity,
+            ]);
+      foreach ($recipients as $recipient) {
+        if ($this->granted_service->isGranted($recipient, 'ROLE_MEMBER' )) {
+          $email->addTo($recipient->getEmail());
+        }
+      }
+      $this->mailer->send($email);
+    }
+    if ($entity Instanceof Event) {
+      $email->from('surfcastingturballais@lanco.ovh')
+            ->subject('Un nouvel événement vient d\'être publié sur le site')
+            ->htmlTemplate('email_template/event_notification.html.twig')
+            ->context([
+              'entity' => $entity,
+            ]);
+      foreach ($recipients as $recipient) {
+        if ($this->granted_service->isGranted($recipient, 'ROLE_MEMBER' )) {
+          $email->addTo($recipient->getEmail());
+        }
+      }
+      $this->mailer->send($email);
+    }
+
+    return;
   }
 }
